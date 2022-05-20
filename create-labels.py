031525609c183526903bc76d160510269d77b9d0
misirov <carebymedia@gmail.com>
2022-05-20 22:43:09 +0200

    improved script

--- create-labels.py
+++ create-labels.py
@@ -1,10 +1,13 @@
-# This script removes default github labels and adds custom ones using python requests and the github API
-# To work it needs: 1) User token, 2) Target username / organization name and target repository name and the 
-# By Pablo and Spearbit with <3
-# License: MIT
-
+"""
+This script removes default github labels and adds custom ones using python requests and the github API
+To work it needs:
+ 1) User token 
+ 2) Target username / organization name
+ 3) Target repository name and the 
+By Pablo and Spearbit with <3
+License: MIT
+"""
 import requests
-
 
 def delete_default_labels():
     default_labels = [
@@ -26,46 +29,29 @@ def delete_default_labels():
 
 
 def create_new_labels():
-    new_labels = [
-        "Severity: Critical Risk", 
-        "Severity: High Risk",
-        "Severity: Medium Risk", 
-        "Severity: Low Risk", 
-        "Severity: Informational",
-        "Severity: Gas Optimization",
-        "Status: Acknowledged",
-        "Status: Fixed",
-        "Status: ReadyForReport"
+    labels = [
+        { "name": "Severity: Critical Risk", "color": "ff0000"},
+        { "name": "Severity: High Risk" , "color": "B60205"},
+        { "name": "Severity: Medium Risk" , "color": "D93F0B"},
+        { "name": "Severity: Low Risk", "color": "FBCA04"},
+        { "name": "Severity: Informational" , "color": "1D76DB"},
+        { "name": "Severity: Gas Optimization", "color": "B4E197"},
+        { "name": "Status: Acknowledged", "color": "5319E7"},
+        { "name":  "Status: Fixed", "color": "0E8A16"},
+        { "name": "Status: ReadyForReport", "color": "bfdadc"}
     ]
 
-    colors = [
-        "ff0000",
-        "B60205",
-        "D93F0B",
-        "FBCA04",
-        "1D76DB",
-        "B4E197",
-        "5319E7",
-        "0E8A16",
-        "bfdadc"
-    ]
     i = 0
-    if len(new_labels) == len(colors):
-        for n in new_labels:
-            data={
-                "name":n,
-                "color":colors[i]
-            }
-            print(data)
-            r = requests.post(f"https://api.github.com/repos/{username}/{repo}/labels", headers=headers, json=data)
-            print(r)
-            i+=1
-        print("Done creating new labels!\n")
+    for data in labels:
+        print(data)
+        r = requests.post(f"https://api.github.com/repos/{username}/{repo}/labels", headers=headers, json=data)
+        print(r)
+        i+=1
+    print("Done creating new labels!\n")
 
 
 
 # __ MAIN __
-
 print("Hello! This script will first delete all labels from the target repository and create custom ones. Please enter the following details:\n")
 while(True):
     token = input(str("1) Access token: "))
@@ -77,9 +63,9 @@ while(True):
  
 
 headers = {
-    "Authorization":f"token {token}",
+    "Authorization": f"token {token}",
     "Accept": "application/vnd.github.v3+json",
-    'User-Agent': 'request'
+    "User-Agent": "request"
 }
 
 url = f"https://api.github.com/repos/{username}/{repo}/labels"
@@ -91,10 +77,3 @@ delete_default_labels()
 create_new_labels()
 
 print("Please confirm old labels have been deleted and new ones have been created.")
-
-
-
-
-
-
-
